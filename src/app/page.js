4f4d28d2a954d5affb8ab010efe683fe4916ea6a
B00145512 <B00145512@mytudublin.ie>
2024-04-25 23:36:22 +0100

    nav bar and home page changed
    nav bar has logo on everypage
    homepage is done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+"use client"
 import React from 'react';
 import Link from 'next/link';
 import { Container, Typography, Button, Grid,  AppBar, Toolbar, } from '@mui/material';
 import CustomAppBar from './components/ResponsiveAppBarHome'; // Import the AppBar component
+import { useState, useEffect } from 'react';
+import Cookies from 'universal-cookie';
 
 const HomePage = () => {
+
+  const [username, setUsername] = useState('Guest');
+  const cookies = new Cookies();
+
+  useEffect(() => {
+    const savedUsername = cookies.get('nick');
+    if (savedUsername) {
+      setUsername(savedUsername);
+    }
+  }, []);
+
   const backgroundStyle = {
     backgroundImage: `url(${'/HomePageBG.png'})`,
     backgroundSize: 'cover', 
@@ -13,24 +27,60 @@ const HomePage = () => {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'left',
-    textAlign: 'ledf',
+    textAlign: 'left',
     padding: '50px',
     margin: '-8px',
     overflow: 'hidden',
+  };
+  const titleStyle = {
+    color: 'White',
+    marginBottom: '20px',
+    fontFamily: 'Impact, sans-serif', 
+    fontSize: '150px',
+    marginLeft: '-200px',
+  };
+
+  const subtitleStyle = {
+    color: '#fff',
+    fontSize: '50px',
+    maxWidth: '600px',
+    lineHeight: '1.5',
+    fontFamily: 'Impact, sans-serif',
+    marginTop: '-190px',
+    marginLeft: '200px',
   };
 
   return (
     <div style={backgroundStyle}>
       <CustomAppBar />
       <Container>
-        <Typography variant="h2" gutterBottom>
+      <Typography variant="h2" gutterBottom style={titleStyle}>
           Task Titans
-          <hr/>
+          <br/>
+          <hr style={{backgroundColor: 'White', width: '900px', height: '5px', margin: '-10px'}}/>
+          <br/>
+        </Typography>
+        <Typography variant="h4" gutterBottom style={subtitleStyle}>
+          Welcome to Task Titans!
         </Typography>
         <Typography variant="body1" gutterBottom>
-          Master Your Tasks and become a Titan
+        <br/><br/>
+          <h1 style={{color: 'white'}}>Master Your Tasks and become a Titan!</h1><br/>
+          <hr style={{width: '700px', margin: '-10px' }}/>
+          <br/>
+           This app is designed to help you manage your tasks and time effectively.
+           <br/> Please enjoy the app and let us know if you have any feedback!
         </Typography>
-       
+        <Typography>
+          
+          <Link href="/todolist" passHref>
+            <Button variant="contained" color="primary" style={{ marginTop: '30px' }}>
+            {username}!
+            <br/>
+              Lets Do Some Tasks!
+            </Button>
+          </Link>
+        </Typography>
       </Container>
     </div>
   );
